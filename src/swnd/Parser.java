bacd7b867c8250b1ca795270c5f6ae7626064b70
cmos <m.p.kowalski.13@stud.elka.pw.edu.pl>
2015-12-22 18:03:54 +0100

    reformat

--- src/swnd/Parser.java
+++ src/swnd/Parser.java
@@ -2,16 +2,14 @@ package swnd;
 
 import edu.asu.emit.algorithm.graph.Graph;
 import edu.asu.emit.algorithm.graph.Path;
-import edu.asu.emit.algorithm.graph.shortestpaths.DijkstraShortestPathAlg;
+//import edu.asu.emit.algorithm.graph.shortestpaths.DijkstraShortestPathAlg;
 import edu.asu.emit.algorithm.graph.shortestpaths.YenTopKShortestPathsAlg;
 
 import java.util.List;
 
-/**
- * Created by cmos on 17.12.15.
- */
+
 public class Parser {
-    private Graph graph = null;
+
 
     Integer[] compute(int a, int b, int c) {
         Graph graph = new Graph("data/v4");
@@ -23,25 +21,27 @@ public class Parser {
         //Object x = shortest_paths_list.toArray();
         //System.out.println(yenAlg.getResultList().size());
 
-        if (shortest_paths_list.size() < c){
+        if (shortest_paths_list.size() < c) {
             throw new ExceptionInInitializerError();
         }
 
 
-        String path = shortest_paths_list.get(c-1).getVertexList().toString();
-            String[] tokens = path.split(":");
-            String[] items = tokens[0].replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(" ", "").split(",");
+        String path = shortest_paths_list.get(c - 1).getVertexList().toString();
+        String[] tokens = path.split(":");
+        String[] items = tokens[0].replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(" ", "").split(",");
 
-            Integer[] results = new Integer[items.length];
+        Integer[] results = new Integer[items.length];
 
-            for (int i = 0; i < items.length; i++) {
-                try {
-                    results[i] = Integer.parseInt(items[i]);
-                    //System.out.println(results.toString());
-                } catch (NumberFormatException nfe) {
-                }
+        for (int i = 0; i < items.length; i++) {
+            try {
+                results[i] = Integer.parseInt(items[i]);
+                System.out.print(results[i].toString());
+                System.out.println();
+            } catch (NumberFormatException nfe) {
+                System.err.println(nfe.toString());
             }
-            return results;
+        }
+        return results;
 
 
     }
